b71b066ef9ee3371a6a72adaf5d077790836ea5c
Jerome Bove <jerome.bove@camptocamp.com>
2010-12-01 10:10:06 +0100

    Ssh keys are now stored in $backuppc_ssh_prv_key and $backuppc_ssh_pub_key

--- manifests/classes/backuppc.pp
+++ manifests/classes/backuppc.pp
@@ -43,45 +43,19 @@ class backuppc {
     group => backuppc,
     mode => "0755",
   }
+
   file {"/var/lib/backuppc/.ssh/id_rsa":
     ensure => present,
-    content => '-----BEGIN RSA PRIVATE KEY-----
-MIIEogIBAAKCAQEA16EyAo8oosOkTdEytXUnxxhMGFFADSYjVlms9E1RI086o7/I
-PsApdZZhDoXmSPKRkjeLIH+nmnDErWX4gkJtSP+qdDKLho3xpTa7WFYrcxZW4een
-6b9bcHs+ydUTAcNCuS0Hed/Bd2XmX19G8tGa1z5GKsiyGSULvKgG0fmNxZdukuOW
-V1A0tNpdzOuoQBZZvWNCSVRs+ta6eDD8upSGJZqDqo6st99mLbhj2tChbgrisSwv
-ZAaTLi3RJr8v5GP3tqPlbLVEPtFib7fC0mqaT/ceuHGFg8y0MjH/rnrjhjyBiLKC
-KMd80WjXmQYWJoya2lWXxwINa1DW9Pxw/4LGpQIBIwKCAQAGKS1Qh79VG4hZ/qmt
-ab9O1M75YWg649UfuWtXb+xgGDTgG2wfDM38C5xfgCsmp9hGAZZCwdGWssPKcKCz
-Q7n6xXnmEBKdccUTWVXPUutMblpA9/17tQKc0FJAR+qZpn4/zhYoDbUSCjnINewV
-kPXLorGMMZ6wQuMUBMz+r1vScf0K//RdSy1yRcjMtH+L3LCm6CRvLtA/ThIxv97s
-MzxWfM8EuundSj6JJpLDR89HxhKbGwlVkAkac74p4DKX7k3OCE9Bc8vcEky1IslK
-NmpYfoF1tQSXQD/8wJXmcPGTWpfngD3RD8Gtan66WzZNS+mWHvt2nP3KIcHJ3suH
-q8ELAoGBAPFgvsBH6QWznzEaCcYCydy8SbRFe+qyCgUZ0ivO8cQ10LbDZKF+NQwM
-PjPyqgOrwrsRTSBTC6ZTgMdfktKYHoxW2ejYvIW08NZRLAsdF97dKViMUZ+SSzU4
-c30Z2dPxwf3JgimQTHG0PN8E6WVYfniXzDGbbqcAgDPTr74d21EFAoGBAOSxJm1O
-H/hhsLi2NHIeIFYvXJLQcgmhIDiRI1agoYIcuX9BnBH4g81hNB81VUbyM5xrLMhe
-Sa482goNhXKGsMg0pyahI99zYA9ayjKP4jFZghMUYEVDuL9ragQTMAhvF78Yp+UD
-U5BnnKUZpEQ8rTK8TQT0HLPIMySH1t9VLBEhAoGBAOOVuypSbf4PwfsYjN9EdS81
-L4y2isdXaIh3bmPRv2E6De4l8SqM8C/uSUbsHKRgHgDVzGBOTNAFlq1aHLfugzPc
-3BYOLheqme6kThkbbkcoS44ApLsGRukJVvJLkt3Gr5eDerl5bKW4kSoL8gB4Ajcw
-CasdhZYlCymNE2ocJo43AoGAdZzvMOZZla6VZk8TqGdD1I1izylfOCb6oL+rxia5
-dh1mtnmDdvTWEdo4EAzMxWaeMy/N42Ow3UPd2UjISYcZFoF6iOUoZEn241NDanXi
-CsBRhie8e2SoJ+4ZQ+ycXBvgU6ZHt5tPjBgHatn8tWEInb/tGH2LGqF5Yz6Lw1Bf
-zksCgYEAkjoDF8a+lkIwtlE1g9CxKRI0aeyGLXsK0YaAr3wDFLC4SudgvEKBA4NB
-9oJSq9kfcQS/SNaju8O9J1JcKvad9mCy/4q4GMiSo7HUJy2m2HUGLKuV0nWPC4TE
-Q5OPobsspX75eNZ0m4cO4t9mjJhD5o0BuaWp43Deavypu5paEhI=
------END RSA PRIVATE KEY-----',
+    content => $backuppc_ssh_prv_key,
     owner => backuppc,
     group => backuppc,
     require => File["/var/lib/backuppc/.ssh"],
     mode    => "0600",
   }
 
-
   file {"/var/lib/backuppc/.ssh/id_rsa.pub":
     ensure => present,
-    content => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA16EyAo8oosOkTdEytXUnxxhMGFFADSYjVlms9E1RI086o7/IPsApdZZhDoXmSPKRkjeLIH+nmnDErWX4gkJtSP+qdDKLho3xpTa7WFYrcxZW4een6b9bcHs+ydUTAcNCuS0Hed/Bd2XmX19G8tGa1z5GKsiyGSULvKgG0fmNxZdukuOWV1A0tNpdzOuoQBZZvWNCSVRs+ta6eDD8upSGJZqDqo6st99mLbhj2tChbgrisSwvZAaTLi3RJr8v5GP3tqPlbLVEPtFib7fC0mqaT/ceuHGFg8y0MjH/rnrjhjyBiLKCKMd80WjXmQYWJoya2lWXxwINa1DW9Pxw/4LGpQ==',
+    content => $backuppc_ssh_pub_key,
     owner => backuppc,
     group => backuppc,
     require => File["/var/lib/backuppc/.ssh"]
@@ -98,12 +72,17 @@ Q5OPobsspX75eNZ0m4cO4t9mjJhD5o0BuaWp43Deavypu5paEhI=
   @@ssh_authorized_key {"$fqdn":
     ensure => present,
     type => ssh-rsa,
-    key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA16EyAo8oosOkTdEytXUnxxhMGFFADSYjVlms9E1RI086o7/IPsApdZZhDoXmSPKRkjeLIH+nmnDErWX4gkJtSP+qdDKLho3xpTa7WFYrcxZW4een6b9bcHs+ydUTAcNCuS0Hed/Bd2XmX19G8tGa1z5GKsiyGSULvKgG0fmNxZdukuOWV1A0tNpdzOuoQBZZvWNCSVRs+ta6eDD8upSGJZqDqo6st99mLbhj2tChbgrisSwvZAaTLi3RJr8v5GP3tqPlbLVEPtFib7fC0mqaT/ceuHGFg8y0MjH/rnrjhjyBiLKCKMd80WjXmQYWJoya2lWXxwINa1DW9Pxw/4LGpQ==',
+    key => $backuppc_ssh_pub_key,
     tag => "backuppc",
     user => "root"
   }
 
-  Line <<| tag == "backuppc-client" |>> 
+  common::concatfilepart {"000-backuppc.hosts":
+    file => "/etc/backuppc/hosts",
+    content => "host        dhcp    user    moreUsers     # <--- do not edit this line\n",
+  }
+
+  Common::Concatfilepart <<| tag == "backuppc-client" |>> 
 
   service {"backuppc":
     ensure => running,
